8d5dc6208bd04ef33f057800c37a84d893736eac
ReikaIzumi <aaa.1829.88@gmail.com>
2018-03-12 15:31:09 +0800

    [WIP]ファイルの整理、へったーフッターhtmlほぼ完成 りくワイヤのリンクも作成済みリンク

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
-
 <!-- フッター -->
 <!-- アバウトtemecharlotte -->
         <div class="module-small bg-dark">
           <div class="container">
             <div class="row">
-              <div class="col-sm-3">
+              <!-- <div class="col-sm-3">
                 <div class="widget">
                   <h5 class="widget-title font-alt">About charlotte</h5>
                   <p>The languages only differ in their grammar, their pronunciation and their most common words.</p>
@@ -12,47 +11,17 @@
                   <p>Fax: +1 234 567 89 10</p>
                   <p>Email: <a href="#">info@joinus.com</a></p>
                 </div>
-              </div>
+              </div> -->
 
 
 <!-- サイトマップ設定 -->
               <div class="col-sm-3">
                 <div class="widget">
-                  <h5 class="widget-title font-alt">Settings</h5>
+                  <h5 class="widget-title font-alt">Site map</h5>
                   <ul class="icon-list">
-                    <li><a href="#">Designer Desk Essentials</a></li>
-                    <li><a href="#">Realistic Business Card Mockup</a></li>
-                  </ul>
-                </div>
-              </div>
-
-
-<!-- サイトマッププラン　作成、検索、一覧 -->
-              <div class="col-sm-3">
-                <div class="widget">
-                  <h5 class="widget-title font-alt">Plan</h5>
-                  <ul class="icon-list">
-                    <li><a href="#">Photography - 7</a></li>
-                    <li><a href="#">Web Design - 3</a></li>
-                    <li><a href="#">Illustration - 12</a></li>
-                  </ul>
-                </div>
-              </div>
-
-<!-- サイトマップ リクエスト　作成、検索、一覧 -->
-              <div class="col-sm-3">
-                <div class="widget">
-                  <h5 class="widget-title font-alt">Request</h5>
-                  <ul class="icon-list">
-                    <li><a href="#">Photography - 7</a></li>
-                    <li><a href="#">Web Design - 3</a></li>
-                    <li><a href="#">Illustration - 12</a></li>
-                  </ul>
-                </div>
-              </div>
-
-              
-                    </li>
+                    <li><a href="#">Create plan</a></li>
+                    <li><a href="#">Create request</a></li>
+                    <li><a href="#">Help</a></li>
                   </ul>
                 </div>
               </div>
@@ -77,28 +46,3 @@
             </div>
           </div>
         </footer>
-      </div>
-      <div class="scroll-up"><a href="#totop"><i class="fa fa-angle-double-up"></i></a></div>
-    </main>
-
-
-
-    <!--  
-    JavaScripts
-    =============================================
-    -->
-    <script src="../assets/lib/jquery/dist/jquery.js"></script>
-    <script src="../assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="../assets/lib/wow/dist/wow.js"></script>
-    <script src="../assets/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
-    <script src="../assets/lib/isotope/dist/isotope.pkgd.js"></script>
-    <script src="../assets/lib/imagesloaded/imagesloaded.pkgd.js"></script>
-    <script src="../assets/lib/flexslider/jquery.flexslider.js"></script>
-    <script src="../assets/lib/owl.carousel/dist/owl.carousel.min.js"></script>
-    <script src="../assets/lib/smoothscroll.js"></script>
-    <script src="../assets/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
-    <script src="../assets/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
-    <script src="../assets/js/plugins.js"></script>
-    <script src="../assets/js/main.js"></script>
-  </body>
-</html>
---
+++ register/check.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <h2>送信データ</h2>
+  <?php
+
+  // $_POST
+    echo '<pre>';
+    echo '$_POST=';
+    echo var_dump($_POST);
+    echo '</pre>';
+
+
+   // PHPプログラムでデータの受け取りをします
+    
+
+  ?>
+</body>
+</html>
